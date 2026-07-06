d2e47b8b07829dc35beb16968f3aa22523e362ee
Ethan Miles Scott <ethanscott1142@gmail.com>
2026-07-06 13:26:28 -0400

    Fixed auth-client.ts again

--- auth-client.ts
+++ auth-client.ts
@@ -1,6 +1,5 @@
 'use client';
 
 import { createAuthClient } from 'better-auth/client';
-import type { Auth } from './auth';
 
-export const { useSession, signIn, signUp, signOut } = createAuthClient<Auth>();
+export const { useSession, signIn, signUp, signOut } = createAuthClient();
